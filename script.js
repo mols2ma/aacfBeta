6d92c3e6bbf6ea8058039564ee572dd36efde1ae
drewskiii <lorandrewed@gmail.com>
2019-06-30 15:26:42 -0500

    test

---
+++ script.js
@@ -0,0 +1,9 @@
+// $('.carousel').carousel({
+//   interval: 3000,
+//   pause: "false"
+// });
+
+// $(document).ready(function(){
+//    var height = $(window).height();  //getting windows height
+//    jQuery('#myCarousel').css('height',height+'px');   //and setting height of carousel
+// });
